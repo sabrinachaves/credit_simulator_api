253f8b335cddae69e058522190143239c1d224dc
Sabrina Chaves <sabrinach08@gmail.com>
2024-09-30 17:06:26 -0300

    test: remove duplicated test

--- tests/e2e/Create.test.ts
+++ tests/e2e/Create.test.ts
@@ -10,29 +10,11 @@ describe('POST /v1/simulate/', () => {
       .post(`/simulate/`)
       .send({
         amount: 10000,
-        paymentTermInMonths: 36,
+        paymentTerm: 36,
         birthDate: '2000-12-05',
       })
       .expect(OK);
-    expect(response.body.paymentTermInMonths).toBe(36);
-    expect(response.body.amountToBePaid).toBe(10789.56);
-    expect(response.body.installmentsValue).toBe(299.71);
-    expect(response.body.amountPaidInInterest).toBe(789.56);
-    expect(response.body.interestRate).toBe(5);
-    expect(response.body).toHaveProperty('id');
-    expect(response.body).toHaveProperty('birthDate');
-  });
-
-  it('should return 200 when create a credit simulation #e2e', async () => {
-    const response = await request()
-      .post(`/simulate/`)
-      .send({
-        amount: 10000,
-        paymentTermInMonths: 36,
-        birthDate: '2000-12-05',
-      })
-      .expect(OK);
-    expect(response.body.paymentTermInMonths).toBe(36);
+    expect(response.body.paymentTerm).toBe(36);
     expect(response.body.amountToBePaid).toBe(10789.56);
     expect(response.body.installmentsValue).toBe(299.71);
     expect(response.body.amountPaidInInterest).toBe(789.56);
@@ -46,22 +28,22 @@ describe('POST /v1/simulate/', () => {
       .post(`/simulate/`)
       .send({
         amount: 10000,
-        paymentTermInMonths: 36,
+        paymentTerm: 36,
         birthDate: '2015-12-05',
       })
       .expect(UNPROCESSABLE_ENTITY);
-    expect(response.body).toEqual({error: "Age cannot be less than 18"});
-  }); 
+    expect(response.body).toEqual({ error: 'Age cannot be less than 18' });
+  });
 
   it('should return 422 when try to create a simulation with age greater than 95 #e2e', async () => {
     const response = await request()
       .post(`/simulate/`)
       .send({
         amount: 10000,
-        paymentTermInMonths: 36,
+        paymentTerm: 36,
         birthDate: '1924-01-01',
       })
       .expect(UNPROCESSABLE_ENTITY);
-    expect(response.body).toEqual({error: "Age cannot be greater than 95"});
-  }); 
+    expect(response.body).toEqual({ error: 'Age cannot be greater than 95' });
+  });
 });
