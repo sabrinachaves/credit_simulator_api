60f048c9a57939e172e314c1387ed1b8526a7758
Sabrina Chaves <sabrinach08@gmail.com>
2024-09-30 10:04:42 -0300

    feat: add database factory

---
+++ src/infrastructure/factories/persistence/DatabaseFactory.ts
@@ -0,0 +1,17 @@
+import { IDatabase } from '@infrastructure/persistence/IDatabase';
+import Database from '@infrastructure/persistence/Database';
+import { AppDataSource } from '../../../database/dataSource';
+
+export default class DatabaseFactory {
+  private static database: IDatabase;
+
+  static async make() {
+    if (this.database) {
+      return this.database;
+    }
+
+    this.database = new Database(AppDataSource);
+    await this.database.connect();
+    return this.database;
+  }
+}
