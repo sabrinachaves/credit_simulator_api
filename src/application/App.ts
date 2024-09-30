3594860c6febd7428a65030fd4612110e438e1ef
Sabrina Chaves <sabrinach08@gmail.com>
2024-09-30 10:05:08 -0300

    feat: add express

---
+++ src/application/App.ts
@@ -0,0 +1,44 @@
+import * as http from 'http';
+import express, { RequestHandler } from 'express';
+import 'reflect-metadata';
+
+interface AppOptions {
+  port: number;
+  routes: express.Router;
+  middlewares?: RequestHandler[];
+  environment: string;
+}
+
+export default class App {
+  app: express.Express;
+  port: number;
+  routes: express.Router;
+  middlewares: RequestHandler[];
+  environment: string;
+
+  constructor(options: AppOptions) {
+    this.app = express();
+    this.port = options.port;
+    this.routes = options.routes;
+    this.middlewares = options.middlewares || [];
+    this.environment = options.environment || '';
+
+    this.handlerMiddlewares();
+    this.handlerRoutes();
+  }
+
+  private handlerRoutes(): void {
+    this.app.use(this.routes);
+  }
+
+  private handlerMiddlewares(): void {
+    this.middlewares.forEach((value) => this.app.use(value));
+  }
+
+  async listen(): Promise<http.Server> {
+    return this.app.listen(this.port, () => {
+      console.log(`Application "credit_simulator_api" is running. Listening on http://localhost:${this.port}`);
+      console.log('Press CTRL+C to exit');
+    });
+  }
+}
